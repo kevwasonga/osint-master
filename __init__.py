1ff555f7862c4e62455065485f80bc546138f348
kevwasonga <kevinwasonga116@gmail.com>
2025-11-28 14:26:29 +0300

    Add root package initialization

---
+++ __init__.py
@@ -0,0 +1,5 @@
+"""OSINT-Master: Multi-function passive OSINT reconnaissance tool."""
+
+__version__ = "1.0.0"
+__author__ = "OSINT-Master Contributors"
+__description__ = "Multi-function passive OSINT reconnaissance tool for cybersecurity learning and threat intelligence"
